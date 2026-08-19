09cab74069aa01ceb136517c56d7c4898de48b26
Satyajit-zx <shitsatyajit29@gmail.com>
2026-08-19 19:39:26 +0530

    Add GFG POTD - Triplets with Sum in Range

---
+++ 2026-08-19_Triplets-with-Sum-in-Range.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    long long countLessEqual(vector<int>& arr, int limit) {
+        int n = arr.size();
+        long long count = 0;
+
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                long long sum = (long long)arr[i] + arr[left] + arr[right];
+
+                if (sum <= limit) {
+                    count += (right - left);
+                    left++;
+                } 
+                else {
+                    right--;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    int countTriplets(vector<int> &arr, int l, int r) {
+        sort(arr.begin(), arr.end());
+
+        long long ans = countLessEqual(arr, r)
+                      - countLessEqual(arr, l - 1);
+
+        return (int)ans;
+    }
+};
