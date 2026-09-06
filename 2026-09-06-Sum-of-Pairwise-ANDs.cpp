6c0b6632115858172f4caa136e452f1c83392ceb
Satyajit-zx <shitsatyajit29@gmail.com>
2026-09-06 20:59:10 +0530

    GFG POTD - Sum of Pairwise ANDs

---
+++ 2026-09-06-Sum-of-Pairwise-ANDs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long pairAndSum(vector<int> &arr) {
+        int n = arr.size();
+        long long ans = 0;
+
+        for (int bit = 0; bit < 31; bit++) {
+            long long cnt = 0;
+
+            for (int x : arr) {
+                if (x & (1LL << bit))
+                    cnt++;
+            }
+
+            ans += cnt * (cnt - 1) / 2 * (1LL << bit);
+        }
+
+        return ans;
+    }
+};
